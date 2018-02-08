509f372ec6231af660cc24df662fd69062e15e35
shendypj18 <shendypj18@gmail.com>
2018-02-08 11:36:37 +0700

    perubahan kelima

--- app/kegiatan.php
+++ app/kegiatan.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class kegiatan extends Model
 {
-    //
+  protected $table = 'Kegiatan';
+  protected $primarykey = 'id_kegiatan';
+  protected $fillable = ['id_kegiatan','nama_kegiatan','deskripsi_kegiatan','periode'];
+  public $timestamps = false;
 }
